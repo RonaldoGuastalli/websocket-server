3235d32f2c7e133a3ce4b34bd4ad26e0405cd16b
rjguastalli <rjguastalli@grupodimed.com.br>
2020-01-29 08:48:01 -0300

    First commit

---
+++ src/test/java/br/com/studies/pocwebsocket/PocWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.studies.pocwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
