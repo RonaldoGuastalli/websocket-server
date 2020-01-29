2a6143c2605b35d8ca286063446b10a4f0d444b9
rjguastalli <rjguastalli@grupodimed.com.br>
2020-01-29 11:53:20 -0300

    alterações no websocketconfig

--- src/main/java/br/com/studies/pocwebsocket/configuration/WebSocketConfig.java
+++ src/main/java/br/com/studies/pocwebsocket/configuration/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new SocketHandler(), "/name");
+        registry.addHandler(new SocketHandler(), "/msg").setAllowedOrigins("*");
 
     }
 }
